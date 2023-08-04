4bba912a9ad14b3f10ee249153996fe8449144ad
yanad <yana.danilova1509@gmail.com>
2023-08-04 09:12:12 +0200

    Initial commit

---
+++ src/main/java/com/in28minuten/springboot/hibernatejpa/HibernateJpaApplication.java
@@ -0,0 +1,13 @@
+package com.in28minuten.springboot.hibernatejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minuten/springboot/hibernatejpa/HibernateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minuten.springboot.hibernatejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
